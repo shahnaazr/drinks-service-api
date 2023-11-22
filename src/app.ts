7dcfacd2a1be101cb330eec5f364abc67d71a923
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2023-11-22 20:39:14 +0000

    refactor: modify GET coffee endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { router } from "./routes/routes";
+
 export const app = express();
 app.use(express.json());
 
@@ -6,9 +8,4 @@ app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
 
 app.get("/coffeelover", (req, res) => res.send("I love coffee!"));
 
-app.get("/coffee", (req, res) => {
-  res.json({
-    drinkType: "Coffee",
-    name: "Latte",
-  });
-});
+app.use("/", router);
