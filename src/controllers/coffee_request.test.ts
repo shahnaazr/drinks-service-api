3bab2c213e1ba7f44ac9bb87e333fccf5b33d32d
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2023-11-22 21:43:07 +0000

    test: add additional tests for GET coffee endpoint

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -9,4 +9,18 @@ describe("Test coffee endpoint request", () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({ drinkType: "Coffee", name: "Latte" });
   });
+
+  test("GET coffee endpoint with query parameter, Cappacino should return the correct json response", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Cappacino" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ drinkType: "Coffee", name: "Cappacino" });
+  });
+
+  test("GET coffee endpoint without query parameter should return the correct json response", async () => {
+    const res = await request(app).get("/coffee");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ drinkType: "Coffee", name: "Latte" });
+  });
 });
